50328346e274ba09f331f3a414ac546e98afd24c
Pheodaron <grindar10101@gmail.com>
2022-05-16 15:08:35 +0300

    ~

--- src/context/AuthContext/index.js
+++ src/context/AuthContext/index.js
@@ -2,7 +2,6 @@ import { createContext } from "react";
 
 export const AuthContext = createContext({
     isLoaded: false,
-    isLoggedIn: () => {},
     user: null,
     token: null,
     setIsLoaded: () => {},
